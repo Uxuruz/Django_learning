2ec95eac0f6804c757b7089c2e82df07fa1baaa9
Uxuruz(Dario Castro) <decs1183@gmail.com>
2026-06-24 20:30:52 -0600

    Simplificando logica de forma dinamica

--- quote/views.py
+++ quote/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound # Importar 
 
+#Diccionarios dias de la semana
+
+days_of_week = {
+    "monday": "Hola monday",
+    "tuesday": "hola tuesday",
+    "wednesday": "hola wednesday",
+    "thursday": "Hola Thursdat",
+    "friday": "Helo Friday",
+    "saturday": "hola saturday",
+    "sunday": "hola sundays",
+}
+
 # Create your views here.
-def days_week_int(request, day):
-    pass           
+def days_week_int(request, day):     
     return HttpResponse(day)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 
 #Para crear vistas dinamicas 
 def days_week(request, day):
-    quote_text = None
-    if day == "monday":
-        quote_text = "Hola Lunes :D"
-    elif day == "tuesday":
-        quote_text = "Hola Tuesdaday :D" 
-    else:
-        return HttpResponseNotFound("Day not found")     
-    return HttpResponse(quote_text)
+    try : #Manejo de errores
+        quote_text = days_of_week[day]
+        return HttpResponse(quote_text)
+    except:
+       return HttpResponseNotFound("Day not found")     
+    
