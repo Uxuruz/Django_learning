56344edd56e04d4da7f961fdaefc7c3bacbea7af
Uxuruz(Dario Castro) <decs1183@gmail.com>
2026-06-27 17:14:29 -0600

    Filtro Fechas

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse # Importamos para poder usar httpresponse
-
+from datetime import date
 # Create your views here.
 def home(request):
+    today = date.today()
     return render(request, "landing/landing.html",{
         "name": "Dario Castro",
-        "name2": "Elias"
+        "name2": "Elias",
+        "today": today
     })
