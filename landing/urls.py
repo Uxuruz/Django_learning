975c32f7962d99f0079c5d4493a0679e867ca816
Uxuruz(Dario Castro) <decs1183@gmail.com>
2026-06-28 14:53:58 -0600

    urls dinamicas y url tags

--- landing/urls.py
+++ landing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views # . porque se encuentra en el mismo directorio y enlazamos views con home
 
 urlpatterns = [
-    path('home', views.home , name="home")
+    path('home', views.home , name="home"),
+    path('stack/<str:tool>', views.stack_details, name = "stack" ) #vita stack que sea string y utiliza variable tool
 ]
--- landing/views.py
+++ landing/views.py
@@ -4,10 +4,19 @@ from datetime import date
 # Create your views here.
 def home(request):
     today = date.today()
-    stack = ["Python","Dart","PHP","flutter","Django"]
+    stack = [
+        {'id': 'python', 'name': "Python"},
+        {'id': 'dart', 'name': "Dart"},
+        {'id': 'php', 'name': "PHP"},
+        {'id': 'flutter', 'name': "Flutter"},
+        {'id': 'django', 'name': "Django"}
+    ]
     return render(request, "landing/landing.html",{
         "name": "Dario",
         "name2": "Elias",
         "today": today,
         "stack": stack
     })
+
+def stack_details(request, tool):  
+  return HttpResponse(f"Tecnologia {tool}") 
